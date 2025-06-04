bfcee6c0a375701e41eb111472b9231ff5861547
Checkmate1941 <adriaan.vanderberg@outlook.com>
2025-06-04 15:23:00 +0800

    flashcards

--- flashcards_CV.js
+++ flashcards_CV.js
@@ -1,5 +1,5 @@
 // flashcards.js
-const flashcards = [
+const flashcard = [
     /* ───────────── Lecture 2 – Image Basics, Thresholding & Morphology ───────────── */
     {
     "category": "Lecture02(Binary Image Analysis)",
